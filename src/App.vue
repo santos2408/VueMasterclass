3abbccd9898bf8c3aa819516a7a18d5769f97359
Roger Santos <roger.santos36@gmail.com>
2022-05-24 18:52:21 -0300

    deleted unneeded files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="I will be the fucking Master of Vue.js" />
-  <h1>Hello, mother fucker</h1>
+  <h1>Hello, Vue</h1>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
-
-<style scoped>
-#app {
-  background-color: red;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1 {
-  color: red;
-}
-</style>
