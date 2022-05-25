95d5711cffc1fa460f003b945b2e927c8e00c7de
Roger Santos <roger.santos36@gmail.com>
2022-05-25 16:35:58 -0300

    reset modifications

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>HEADER</div>
   <h1>Hello, Vue</h1>
-
-  <nav class="menu-nav">
-    <ul class="menu-list">
-      <li class="menu-item">Home</li>
-      <li class="menu-item">Portfolio</li>
-      <li class="menu-item">Contact</li>
-    </ul>
-  </nav>
 </template>
 
 <script>
