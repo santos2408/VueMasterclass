633c917cfb311aa4ffae4a430d4c7dd007c9b898
Roger Santos <roger.santos36@gmail.com>
2022-05-25 18:26:33 -0300

    add todo comentaries

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,4 @@ export default {
 </script>
 
 <!-- TODO: criar uma branch para cada componente desenvolvido -->
+<!-- TODO: desenvolver consultando a docs do tailwind e o resposito sobre padrões de commit do iuri - codelandia -->
